e80939da9717ff0502a611c7a323f37666d99b77
Gautam Iyer <gautam@math.cmu.edu>
2015-04-13 17:29:15 -0400

    Added production option

--- build.py
+++ build.py
@@ -1,36 +1,44 @@
 #! /usr/bin/python
 
 from staticjinja import make_site
-import re
-
 from jinja2_markdown import MarkdownExtension
+import re
+import optparse
+import os
 
 if __name__ == "__main__":
-    partial_re = re.compile( '(?:^|.*/)_' ) 
-    ignored_re = re.compile( '.*\.(?:swp|un~)$', flags=re.I )
-    static_re  = re.compile(
-	    'static/(?!.*\.(:?swp|un~)$)|.*\.(:?pdf|jpg|png|svg|eps|ps)$',
-	    flags=re.I )
 
-    def is_partial( filename ):
-	return True if partial_re.match( filename ) else False
+    # Options.
+    parser = optparse.OptionParser()
 
-    def is_ignored( filename ):
-	return True if ignored_re.match( filename ) else False
+    # Use -p for production. The global dev_env is set to 'local' or
+    # 'production' respectively.
+    parser.add_option( '-p', '--production', dest='production',
+	    action='store_true', help='Generate site for production' )
 
-    def is_static( filename ):
-	return True if static_re.match( filename ) else False
+    ( options, args ) = parser.parse_args()
 
     site = make_site(
 	    searchpath='./src',
-	    outpath='./out',
+	    outpath='./out-prod' if options.production else './out',
 	    extensions=[MarkdownExtension],
 	)
 
+    # Tweak the Jinja2 environment
     site._env.line_statement_prefix = '<@Jinja2>'
-    site.is_ignored = is_ignored
-    site.is_partial = is_partial
-    site.is_static = is_static
+    site._env.globals['dev_env'] = \
+	'production' if options.production else 'local'
+
+    ignored_re = re.compile( '.*\.(?:swp|un~)$', flags=re.I )
+    site.is_ignored = lambda f: True if ignored_re.match( f ) else False
+
+    partial_re = re.compile( '(?:^|.*/)_' ) 
+    site.is_partial = lambda f: True if partial_re.match( f ) else False
+
+    static_re  = re.compile(
+	    'static/(?!.*\.(:?swp|un~)$)|.*\.(:?pdf|jpg|png|svg|eps|ps)$',
+	    flags=re.I )
+    site.is_static = lambda f: True if static_re.match( f ) else False
 
     # enable automatic reloading
     site.render(use_reloader=False)
