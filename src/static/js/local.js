1513f30a8fe7d1ab1ffdb471aa8b0061d66fe4ca
Gautam Iyer <gautam@math.cmu.edu>
2015-09-03 22:37:42 -0400

    Got affix working without offsets

--- src/static/js/local.js
+++ src/static/js/local.js
@@ -25,4 +25,40 @@ $(document).ready( function() {
       $(window).resize(resizeFn);
   });
 
+
+  /* Easy affix:
+   *
+   *	<div id='before-affix'></div>
+   *	<div data-affix-after='#before-affix'>
+   *	    Content to affix
+   *	</div>
+   *
+   * and don't worry about offsets or widths.
+   */
+  $('[data-affix-after]').each( function() {
+      var elem = $(this);
+      var prev = $( elem.data('affix-after') );
+      var parent_panel = elem.parent();
+
+      var resizeFn = function() {
+	  elem.data('bs.affix').options.offset
+	      = prev.offset().top + prev.outerHeight(true);
+
+	  elem.affix( 'checkPosition' );
+
+	  var sideBarNavWidth = parent_panel.width()
+	      - parseInt(elem.css('paddingLeft'))
+	      - parseInt(elem.css('paddingRight'))
+	      - parseInt(elem.css('marginLeft'))
+	      - parseInt(elem.css('marginRight'))
+	      - parseInt(elem.css('borderLeftWidth'))
+	      - parseInt(elem.css('borderRightWidth'));
+	  elem.css('width', sideBarNavWidth);
+      };
+
+      elem.affix();
+      resizeFn();
+      $(window).resize(resizeFn);
+  });
+
 }); /* end ready */
