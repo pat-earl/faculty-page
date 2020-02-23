82dc3dff81843e7e974f739120f6405be0a33423
Gautam Iyer <gautam@math.cmu.edu>
2020-02-23 10:54:38 -0500

    Gave faculty trust 2, everyone trust 1 by default

--- src/zym/sso.php
+++ src/zym/sso.php
@@ -28,16 +28,20 @@ $userEmail = getenv( 'eppn' );
 // Required and must be unique to your application
 $userId = strstr( $userEmail, '@', true );
 
-
 // Optional - if you don't set these, Discourse will generate suggestions
 // based on the email address
 /* $extraParameters = array(
     'username' => $userUsername,
     'name'     => $userFullName
   ); */
+$extraParameters = array();
+$extraParameters['add_groups'] = 'trust_level_0,trust_level_1';
+if( strpos( getenv('affiliation'), 'Faculty@andrew.cmu.edu' ) !== false )
+  $extraParameters['add_groups'] .= 'trust_level_2';
+
 
 // build query string and redirect back to the Discourse site
-$query = $sso->getSignInString($nonce, $userId, $userEmail );
+$query = $sso->getSignInString($nonce, $userId, $userEmail, $extraParameters );
 header('Location: https://zym.math.cmu.edu/session/sso_login?' . $query);
 exit(0);
 ?>
