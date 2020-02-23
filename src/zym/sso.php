4b46d1e404648bb9986095a74ef5e69917b38e30
Gautam Iyer <gautam@math.cmu.edu>
2020-02-23 10:53:35 -0500

    Discourse SSO

---
+++ src/zym/sso.php
@@ -0,0 +1,43 @@
+<?php
+require_once( 'SSOHelper.php' );
+
+$sso = new SSOHelper();
+
+// this should be the same in your code and in your Discourse settings:
+// $secret = 'same secret set in discourse';
+require_once( 'secret.php' ); # Sets $secret
+$sso->setSecret( $secret );
+
+// load the payload passed in by Discourse
+$payload = $_GET['sso'];
+$signature = $_GET['sig'];
+
+// validate the payload
+if (!($sso->validatePayload($payload, $signature))) {
+    // invaild, deny
+    header("HTTP/1.1 403 Forbidden");
+    echo("Bad SSO request");
+    die();
+}
+
+$nonce = $sso->getNonce($payload);
+
+// Required and must be consistent with your application
+$userEmail = getenv( 'eppn' );
+
+// Required and must be unique to your application
+$userId = strstr( $userEmail, '@', true );
+
+
+// Optional - if you don't set these, Discourse will generate suggestions
+// based on the email address
+/* $extraParameters = array(
+    'username' => $userUsername,
+    'name'     => $userFullName
+  ); */
+
+// build query string and redirect back to the Discourse site
+$query = $sso->getSignInString($nonce, $userId, $userEmail );
+header('Location: https://zym.math.cmu.edu/session/sso_login?' . $query);
+exit(0);
+?>
