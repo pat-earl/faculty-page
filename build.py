ec090aff4dee14976cc0c5c9a38dcede94665e6f
Patrick Earl <patrick.earl95@gmail.com>
2021-01-11 23:28:36 -0500

    more stuff

--- build.py
+++ build.py
@@ -12,8 +12,8 @@ from stat import *
 from jinja2 import contextfunction, contextfilter
 
 import mdconverter
-
 import pypandoc
+import docker
 
 import coloredlogs
 
@@ -258,6 +258,22 @@ def slide_convert(site, template, **context):
 
     # Copy the permissions from 
     shutil.copymode(template.filename, filepath)
+
+    # Check if in production, if so create the PDF versions
+    if site.options.production:
+
+        client = docker.from_env()
+        client.containers.run(
+            image="astefanutti/decktape",
+            remove=True,
+            tty=True,
+            volumes = {
+                os.getcwd(): {'bind': '/slides', 'mode': 'rw'},
+                os.path.dirname(filepath): {'bind': '/home/user/', 'mode': 'rw'},
+                os.path.join(os.getcwd(), 'src/share/'): {'bind': '/share', 'mode': 'ro'}
+            },
+            command='/home/user/' + os.path.basename(filepath) + ' ' + os.path.basename(filepath) + '.pdf'
+        )
 
 if __name__ == "__main__":
     # Options.
---
+++ src/teaching/2020-2021/252-unix-admin/lectures.md
@@ -0,0 +1,15 @@
+title: Lectures
+
+{% set lectures = (
+    {"name": "Introduction", "file": "slides/intro.md"},
+    {"name": "UNIX History", "file": "slides/history.md"}
+) %}
+
+## Course Lectures
+
+These are the lecture slides used in the course. You can view them in 
+their slide form or as a "printable" PDF, with each slide on a separate page
+
+{% for lec in lectures %}
+* {{ lec['name'] }} - [Slide Form]({{ get_link(lec['file']) }}) - [PDF Form]({{ get_link(lec['file']) ~ "?print-pdf"}})
+{% endfor -%}
